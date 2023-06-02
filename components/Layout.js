5e6b7791fc394fa98121a10217f0c14c9e0488fc
CDarling25 <135234079+CDarling25@users.noreply.github.com>
2023-06-02 14:55:06 -0400

    made changes to Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -2,9 +2,12 @@ import Link from "next/link";
 import Spacer from "./Spacer";
 
 const linkStyle = {
-  marginRight: 15,
+  marginRight: 5,
+  marginLeft: 5,
+  paddingRight: '2.5%',
   textDecoration: "none",
-  color: "black",
+  color: "#4F4B4A",
+  fontFamily: 'Geologica'
 };
 
 const header = {
@@ -16,6 +19,7 @@ const header = {
   height: "1rem",
   backgroundColor: "#CC5E21",
   color: "white",
+  textAlign: 'right'
 };
 
 const body= {
@@ -43,7 +47,12 @@ const instagramLogo = {
 
 const Header = () => (
   <div style={header}>
-    
+    <a style = {linkStyle} href = "/">
+      About
+    </a>
+    <a style = {linkStyle} href = "/projects">
+      Projects
+    </a>
   </div>
 );
 
